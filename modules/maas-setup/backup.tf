51548f6ce508db215888fffe6b4168023b10a134
Stamatis Katsaounis <stamatis.katsaounis@canonical.com>
2025-07-28 11:30:07 +0300

    improve wait-for for s3-integrator 1/stable and add support for 2/edge

--- modules/maas-setup/backup.tf
+++ modules/maas-setup/backup.tf
@@ -2,8 +2,41 @@ resource "juju_machine" "backup" {
   count = var.enable_backup ? 1 : 0
 
   model = juju_model.maas_model.name
-  base  = startswith(var.charm_s3_integrator_channel, "1/") ? "ubuntu@22.04" : "ubuntu@24.04"
+  base  = startswith(var.charm_s3_integrator_channel, "2/") ? "ubuntu@24.04" : "ubuntu@22.04"
   name  = "backup"
+}
+
+resource "juju_secret" "s3_credentials" {
+  count = var.enable_backup && startswith(var.charm_s3_integrator_channel, "2/") ? 1 : 0
+
+  name  = "s3-credentials"
+  model = juju_model.maas_model.name
+
+  value = {
+    "access-key" = var.s3_access_key
+    "secret-key" = var.s3_secret_key
+  }
+  info = "Credentials used to access S3 for MAAS and PostgreSQL backup data"
+}
+
+locals {
+  s3_credentials = (
+    startswith(var.charm_s3_integrator_channel, "2/") ?
+    { "credentials" = "secret:${juju_secret.s3_credentials[0].secret_id}" } :
+    {}
+  )
+}
+
+resource "juju_access_secret" "s3_credentials" {
+  for_each = (
+    var.enable_backup && startswith(var.charm_s3_integrator_channel, "2/") ?
+    juju_application.s3_integrator :
+    toset([])
+  )
+
+  model        = juju_model.maas_model.name
+  applications = [each.value.name]
+  secret_id    = juju_secret.s3_credentials[0].secret_id
 }
 
 resource "juju_application" "s3_integrator" {
@@ -17,18 +50,29 @@ resource "juju_application" "s3_integrator" {
     name     = "s3-integrator"
     channel  = var.charm_s3_integrator_channel
     revision = var.charm_s3_integrator_revision
-    base     = startswith(var.charm_s3_integrator_channel, "1/") ? "ubuntu@22.04" : "ubuntu@24.04"
+    base     = startswith(var.charm_s3_integrator_channel, "2/") ? "ubuntu@24.04" : "ubuntu@22.04"
   }
 
-  config = merge(var.charm_s3_integrator_config, {
-    bucket       = each.value == "maas" ? var.s3_bucket_maas : var.s3_bucket_postgresql
-    tls-ca-chain = length(var.s3_ca_chain_file_path) > 0 ? base64encode(file(var.s3_ca_chain_file_path)) : ""
+  config = merge(var.charm_s3_integrator_config, local.s3_credentials, {
+    bucket = each.value == "maas" ? var.s3_bucket_maas : var.s3_bucket_postgresql
+    tls-ca-chain = (
+      length(var.s3_ca_chain_file_path) > 0 ? base64encode(file(var.s3_ca_chain_file_path)) : ""
+    )
   })
 
   provisioner "local-exec" {
-    # There's currently no way to wait for the charm to be idle, hence the sleep
-    # https://github.com/juju/terraform-provider-juju/issues/202
-    command = "sleep 900;$([ $(juju version | cut -d. -f1) = '3' ] && echo 'juju run' || echo 'juju run-action') ${self.name}/leader sync-s3-credentials access-key=${var.s3_access_key} secret-key=${var.s3_secret_key}"
+    # This is needed until we move to 2/edge s3-integration where the access-key and secret-key are
+    # set with a Juju secret. Currently the Juju provider does not either support wait-for
+    # application or running Juju actions.
+    command = (startswith(var.charm_s3_integrator_channel, "2/") ? "/bin/true" : <<-EOT
+      juju wait-for application ${self.name} --timeout 3600s \
+        --query='forEach(units, unit => unit.workload-status == "blocked" && unit.agent-status=="idle")'
+
+      juju run ${self.name}/leader sync-s3-credentials \
+        access-key=${var.s3_access_key} \
+        secret-key=${var.s3_secret_key}
+    EOT
+    )
   }
 }
 
@@ -43,7 +87,9 @@ resource "juju_integration" "s3_integration" {
   }
 
   application {
-    name     = each.value == "maas" ? juju_application.maas_region.name : juju_application.postgresql.name
+    name = (
+      each.value == "maas" ? juju_application.maas_region.name : juju_application.postgresql.name
+    )
     endpoint = "s3-parameters"
   }
 }
